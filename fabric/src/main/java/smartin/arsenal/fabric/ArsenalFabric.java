e569d0ff5c1b37c8feeec4729dbbc5947cb3df4f
Smartin_b <martinbachmor@gmail.com>
2024-02-03 10:32:12 +0100

    - module stats - mining stat progress - socket+gem start

--- fabric/src/main/java/smartin/arsenal/fabric/ArsenalFabric.java
+++ fabric/src/main/java/smartin/arsenal/fabric/ArsenalFabric.java
@@ -1,11 +1,13 @@
 package smartin.arsenal.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.Event;
 import smartin.arsenal.Arsenal;
 
 public class ArsenalFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Arsenal.init();
+        Event event;
     }
 }
--- forge/src/main/java/smartin/arsenal/forge/ArsenalForge.java
+++ forge/src/main/java/smartin/arsenal/forge/ArsenalForge.java
@@ -1,6 +1,8 @@
 package smartin.arsenal.forge;
 
 import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import smartin.arsenal.Arsenal;
@@ -11,5 +13,7 @@ public class ArsenalForge {
 		// Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(Arsenal.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
             Arsenal.init();
+        Event event;
+        AttackEntityEvent event1;
     }
 }
