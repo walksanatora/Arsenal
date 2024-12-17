6a884d5c88cba2ac18b5d17d021ba98f6abaaf12
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2024-12-17 14:06:36 +0100

    - change modid for arsenal compat

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,2 @@
-## v1.11 (1.20.1)
-- changed katar bc animation
-- rebalanced katana to be less powerfull against high HP targets
-- rebalanced twin handle logic
-- added create option sorting
+## v1.12 (1.20.1)
+- changed mod id to prevent crash with "arsenal"
--- arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
+++ arsenal-common/src/main/java/smartin/arsenal/Arsenal.java
@@ -4,7 +4,7 @@ import dev.architectury.event.events.common.LifecycleEvent;
 
 public class Arsenal
 {
-	public static final String MOD_ID = "arsenal";
+	public static final String MOD_ID = "tm_arsenal";
 
 	public static void init() {
 		LifecycleEvent.SETUP.register(GenerateModularConverters::new);
